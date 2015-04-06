5764de350eb8760d24a9fba9ffea76c980471f9a
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-04-06 22:50:49 +0900

    mocha async test

--- test/mocha-test.js
+++ test/mocha-test.js
@@ -1,11 +1,33 @@
+(function () {
+  'use strict';
 
   var util = require('util');
+
   function ins(x) {
     return util.inspect(x, {colors:true}); }
 
-  var d1 = describe('describe1', function () {
-    var i1 = it('it1', function () {
-    });
-    console.log('i1: ' + ins(i1));
+  describe('describe1', function () {
+    it('it1', function () {
+    }); // it1
+
+    it('it2', function (fn) {
+      setTimeout(fn, 55);
+    }); // it2
+
+    it('it3', function () {
+      return {then: function (res, rej) {
+        setTimeout(res, 40);
+        }};
+    }); // it3
+
+    it('it4', function () {
+      var resolve;
+      setTimeout(function () { resolve(); }, 40);
+      return {then: function (res, rej) {
+        resolve = res;
+        }};
+    }); // it4
+
   });
-  console.log('d1: ' + ins(d1));
+
+})();
