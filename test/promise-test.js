8f3168f1a18b1e22912df00b2466cb81dd07c1ad
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-04-08 01:52:11 +0900

    revise test script

--- test/promise-test.js
+++ test/promise-test.js
@@ -4,8 +4,8 @@
   var assert = require('assert');
 
   var promises = {
-    Promise: Promise,
-    'light-promise': require('light-promise'),
+    Promise: /* native*/ Promise,
+    // 'light-promise': require('light-promise'),
     bluebird: require('bluebird'),
     'promise-light': require('../lib/promise-light')
   };
@@ -41,49 +41,302 @@
 
       if (!Promise) return;
 
-      it('Promise.resolve', function () {
+      it('Promise setup sequence', function () {
+        var seq = 0;
+        assert.equal(++seq, 1);
+        // Promise setup called synchronously
+        // Promise setup は同期的に呼ばれる
+        var p = new Promise(function (resolve, reject) {
+          assert.equal(++seq, 2);
+          resolve(123);
+          assert.equal(++seq, 3);
+        });
+        p.then(
+          function (val) {
+            assert.equal(val, 123);
+            assert.equal(++seq, 5); },
+          function (err) {
+            assert(false, 'ng: ' + err); });
+        assert.equal(++seq, 4);
+        return p;
+      }); // it Promise setup sequence
+
+      it('Promise setup unhandled resolve', function () {
+        return new Promise(function (resolve, reject) {
+          new Promise(function (res, rej) {
+            res(123);
+            resolve('ok');
+          });
+        });
+      }); // it Promise setup unhandled resolve
+
+      it('Promise setup unhandled reject', function () {
+        return new Promise(function (resolve, reject) {
+          new Promise(function (res, rej) {
+            rej(new Error('ng'));
+            resolve('ok');
+          });
+        });
+      }); // it Promise setup unhandled reject
+
+      it('Promise setup resolve then twice', function () {
+        var p = new Promise(function (resolve, reject) {
+          resolve(123);
+        });
+        p.then(
+          function (val) {
+            assert.equal(val, 123); },
+          function (err) {
+            assert(false, 'ng: ' + err); });
+        p.then(
+          function (val) {
+            assert.equal(val, 123); },
+          function (err) {
+            assert(false, 'ng: ' + err); });
+        return p;
+      }); // it Promise setup resolve then twice
+
+      it('Promise setup resolve twice', function () {
+        return new Promise(function (resolve, reject) {
+          resolve(123);
+          resolve(456);
+        }).then(
+          function (val) {
+            assert.equal(val, 123); },
+          function (err) {
+            assert(false, 'ng: ' + err); });
+      }); // it Promise setup resolve twice
+
+      it('Promise setup resolve and reject', function () {
+        return new Promise(function (resolve, reject) {
+          resolve(123);
+          reject(new Error('ng'));
+        }).then(
+          function (val) {
+            assert.equal(val, 123); },
+          function (err) {
+            assert(false, 'ng: ' + err); });
+      }); // it Promise setup resolve and reject
+
+      it('Promise setup reject and resolve', function () {
+        return new Promise(function (resolve, reject) {
+          reject(new Error('ng'));
+          resolve(123);
+        }).then(
+          function (val) {
+            assert(false, 'ng: ' + val); },
+          function (err) {
+            assert.equal(err.message, 'ng'); });
+      }); // it Promise setup reject and resolve
+
+      it('Promise setup reject twice', function () {
+        return new Promise(function (resolve, reject) {
+          reject(new Error('ng'));
+          reject(new Error('ng2'));
+        }).then(
+          function (val) {
+            assert(false, 'ng: ' + err); },
+          function (err) {
+            assert.equal(err.message, 'ng'); });
+      }); // it Promise setup resolve twice
+
+      it('Promise setup error', function () {
+        return new Promise(function (resolve, reject) {
+          throw new Error('ng');
+        }).then(
+          function (val) {
+            assert(false, 'ng: ' + val); },
+          function (err) {
+            assert.equal(err.message, 'ng'); });
+      }); // it Promise setup error
+
+      it('Promise setup unhandled error', function () {
+        var called;
+        new Promise(function (resolve, reject) {
+          throw new Error('ng');
+        }).then(function (val) { assert(false, 'ng: ' + val); })
+        .catch(function (err) { assert.equal(err.message, 'ng'); called = true; } );
+        return delay(10, 'ok').then(
+          function (val) {
+            assert(called, 'called?'); });
+      }); // it Promise setup unhandled error
+
+      it('Promise setup resolve delayed then once', function () {
+        var p = new Promise(function (resolve, reject) {
+          resolve(123);
+        });
+        var called;
+        delay(10, 'ok').then(
+          function (val) {
+            p.then(
+              function (val) {
+                called = true;
+                assert.equal(val, 123); },
+              function (err) {
+                assert(false, 'ng: ' + err); }); },
+            function (err) {
+              assert(false, 'ng: ' + err); });
+        return delay(20, 'ok').then(
+          function (val) {
+            assert(called, 'called?'); });
+      }); // it Promise setup resolve delayed then once
+
+      it('Promise setup resolve delayed then twice', function () {
+        var p = new Promise(function (resolve, reject) {
+          resolve(456);
+        });
+        p.then(
+          function (val) {
+            assert.equal(val, 456); },
+          function (err) {
+            assert(false, 'ng: ' + err); });
+        var called;
+        delay(10, 'ok').then(
+          function (val) {
+            assert.equal(val, 'ok');
+            p.then(
+              function (val) {
+                called = true;
+                assert.equal(val, 456); },
+              function (err) {
+                assert(false, 'ng: ' + err); }); },
+            function (err) {
+              assert(false, 'ng: ' + err); });
+        return delay(20, 'ok').then(
+          function (val) {
+            assert(called, 'called?'); });
+      }); // it Promise setup resolve delayed then twice
+
+      it('Promise.resolve number', function () {
+        return Promise.resolve(123).then(
+          function (val) {
+            assert.equal(val, 123); },
+          function (err) {
+            assert(false, 'ng: ' + err); });
+      }); // it Promise.resolve number
+
+      it('Promise.resolve string', function () {
         return Promise.resolve('ok').then(
-          function (val) { assert(val, 'ok'); });
-      }); // it Promise.resolve
+          function (val) {
+            assert.equal(val, 'ok'); },
+          function (err) {
+            assert(false, 'ng: ' + err); });
+      }); // it Promise.resolve string
 
-      it('Promise.reject', function () {
-        return Promise.reject(new Error('ng')).then(null,
-          function (err) { assert(err.message, 'ng'); });
-      }); // it Promise.reject
+      it('Promise.reject error', function () {
+        return Promise.reject(new Error('ng')).then(
+          function (val) {
+            assert(false, 'ng: ' + val); },
+          function (err) {
+            assert.equal(err.message, 'ng'); });
+      }); // it Promise.reject error
 
-      it('Promise.all', function () {
+      it('Promise.reject non-error', function () {
+        return Promise.reject('ng').then(
+          function (val) {
+            assert(false, 'ng: ' + val); },
+          function (err) {
+            assert.equal(err, 'ng'); });
+      }); // it Promise.reject non-error
+
+      it('Promise.all promises', function () {
         return Promise.all([delay(10, 10), delay(20, 20), delay(30, 30)])
-          .then(function (val) {
-            assert.deepEqual(val, [10, 20, 30]);
-          }, function (err) {
-            assert(false, 'Promise.all fail: ' + err);
-          }); // all
-      }); // it Promise.all
+          .then(
+            function (val) {
+              assert.deepEqual(val, [10, 20, 30]); },
+            function (err) {
+              assert(false, 'ng: ' + err); });
+      }); // it Promise.all promises
 
-      it('Promise.all 2', function () {
+      key !== 'light-promise' &&
+      it('Promise.all []', function () {
+        return Promise.all([])
+          .then(
+            function (val) {
+              assert.deepEqual(val, []); },
+            function (err) {
+              assert(false, 'ng: ' + err); });
+      }); // it Promise.all []
+
+      it('Promise.all resolves', function () {
         return Promise.all([Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)])
-          .then(function (val) {
-            assert.deepEqual(val, [1, 2, 3]);
-          }); // all
-      }); // it Promise.all 2
+          .then(
+            function (val) {
+              assert.deepEqual(val, [1, 2, 3]); },
+            function (err) {
+              assert(false, 'ng: ' + err); });
+      }); // it Promise.all resolves
+
+      key !== 'light-promise' &&
+      it('Promise.all values', function () {
+        return Promise.all([1, 2, 3])
+          .then(
+            function (val) {
+              assert.deepEqual(val, [1, 2, 3]); },
+            function (err) {
+              assert(false, 'ng: ' + err); });
+      }); // it Promise.all values
 
       Promise.race &&
-      it('Promise.race', function () {
-        return Promise.race([delay(10, 10), delay(20, 20), delay(30, 30)])
-          .then(function (val) {
-            assert(val, 10);
-          }, function (err) {
-            assert(false, 'Promise.race fail: ' + err);
-          }); // race
-      }); // it Promise.race
+      it('Promise.race promises', function () {
+        return Promise.race([delay(10, 10), delay(20, 20), delay(15, 30)])
+          .then(
+            function (val) {
+              assert.equal(val, 10); },
+            function (err) {
+              assert(false, 'ng: ' + err); });
+      }); // it Promise.race promises
 
       Promise.race &&
-      it('Promise.race 2', function () {
+      it('Promise.race resolves', function () {
         return Promise.race([Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)])
-          .then(function (val) {
-            assert(val, 1);
-          }); // race
-      }); // it Promise.race 2
+          .then(
+            function (val) {
+              assert.equal(val, 1); },
+            function (err) {
+              assert(false, 'ng: ' + err); });
+      }); // it Promise.race resolves
+
+      Promise.race &&
+      it('Promise.race values', function () {
+        return Promise.race([1, 2, 3]).then(
+          function (val) {
+            assert.equal(val, 1); },
+          function (err) {
+            assert(false, 'ng: ' + err); });
+      }); // it Promise.race values
+
+      Promise.race &&
+      it('Promise.race []', function () {
+        return Promise.race([
+          Promise.race([]).then(
+            function (val) {
+              assert(false, 'ng: ' + val); },
+            function (err) {
+              assert(false, 'ng: ' + err); }),
+          delay(20, 123)]).then(
+            function (val) {
+              assert.equal(val, 123); },
+            function (err) {
+              assert(false, 'ng: ' + err); });
+      }); // it Promise.race []
+
+      it('promise sequence', function () {
+        var seq = 0;
+        assert.equal(++seq, 1);
+        Promise.resolve(1).then(
+          function (val) { assert.equal(val, 1); assert.equal(++seq, 4); },
+          function (err) { assert(false, 'ng: ' + err); ++seq; });
+        assert.equal(++seq, 2);
+        Promise.reject('ng').then(
+          function (val) { assert.equal(val, 1); ++seq; },
+          function (err) { assert.equal(err, 'ng');  assert.equal(++seq, 5); });
+        assert.equal(++seq, 3);
+        return Promise.all([Promise.resolve(1)])
+          .then(
+            function (val) { assert.equal(++seq, 6); },
+            function (err) { assert(false, 'ng: ' + err); ++seq; });
+      }); // it promise sequence
 
     }); // describe
 
