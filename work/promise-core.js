f3f4e15324ede88b124b622d6b7bef156d30afc5
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-11-13 07:28:52 +0900

    tuning

--- work/promise-core.js
+++ work/promise-core.js
@@ -20,82 +20,64 @@
 	var STATE_UNRESOLVED = -1;
 	var STATE_REJECTED = 0;
 	var STATE_RESOLVED = 1;
-	var ARGS_CALLBACK = 2;
-	var ARGS_RESOLVE = 3;
-	var ARGS_REJECT = 4;
-
-	// proto
-	var proto = PromiseCore.prototype;
+	var ARGS_RESOLVE = 2;
+	var ARGS_REJECT = 3;
 
 	function PROMISE_RESOLVE() {}
 	function PROMISE_REJECT() {}
 	function PROMISE_THEN() {}
 
+	// proto
+	var proto = PromiseCore.prototype;
+
+	// initial values into prototype (primitives only)
+	proto.$state = STATE_UNRESOLVED;
+	proto.$result = undefined;
+	proto.$handled = false;
+
 	// PromiseCore
-	function PromiseCore(setup, val, rej, cb, pp) {
-
-		//var $this = function (callback) {
-		//	return new PromiseCore(PROMISE_THEN, null, null, callback, $this);
-		//};
-
+	function PromiseCore(setup, val, res, pp) {
+/*
+		var $this = function (callback) {
+			return new PromiseCore(PROMISE_THEN,
+				function (e) { return e instanceof Error ? callback(e) : callback(Error(e)); },
+				function (v) { return v instanceof Error ? callback(v) : callback(null, v); },
+				$this);
+		};
+		if ($this.constructor !== PromiseCore) {
+			setProto($this, proto);
+//			if ($this.then !== proto.then)
+//				Object.getOwnPropertyNames(proto).forEach(p =>
+//					Object.defineProperty($this, p, {writable: true, configurable: true, value: proto[p]}));
+		}
+*/
 		var $this = this;
 
 		$this.$callbacks = [];
-		//$this.$state = STATE_UNRESOLVED;
-		//$this.$result = undefined;
-		//$this.$handled = false;
 
-		//if ($this.constructor !== PromiseCore)
-		//	setProto($this, proto);
-/*
-		if ($this.constructor !== PromiseCore) {
-			setProto($this, proto);
-
-			if ($this.then !== proto.then)
-				Object.getOwnPropertyNames(proto).forEach(p =>
-					Object.defineProperty($this, p, {writable: true, configurable: true, value: proto[p]}));
+		if (setup === PROMISE_RESOLVE) {
+			$this.$state = STATE_RESOLVED;
+			$this.$result = val;
+			nextTickDo(function () { $this.$fire(); });
 		}
-*/
-
-		if (typeof setup === 'function') {
-
-			if (setup === PROMISE_RESOLVE) {
-				$this.$state = STATE_RESOLVED;
-				$this.$result = val;
-				nextTickDo(function () { $this.$fire(); });
-			}
-			else if (setup === PROMISE_REJECT) {
-				$this.$state = STATE_REJECTED;
-				$this.$result = val;
-				nextTickDo(function () { $this.$fire(); });
-			}
-			else if (setup === PROMISE_THEN) {
-				pp.$callbacks.push([val, rej, cb, resolve, reject]);
-				if (pp.$state !== STATE_UNRESOLVED)
-					nextTickDo(function () { pp.$fire(); });
-				return $this;
-			}
-			else {
-				setup(resolve, reject);
-			}
+		else if (setup === PROMISE_REJECT) {
+			$this.$state = STATE_REJECTED;
+			$this.$result = val;
+			nextTickDo(function () { $this.$fire(); });
+		}
+		else if (setup === PROMISE_THEN) {
+			pp.$callbacks.push([val, res, resolve, reject]);
+			if (pp.$state !== STATE_UNRESOLVED)
+				nextTickDo(function () { pp.$fire(); });
+		}
+		else if (typeof setup === 'function') {
+			setup(resolve, reject);
 		}
 
 		return $this;
 
 		function resolve(val) {
 			if ($this.$state !== STATE_UNRESOLVED) return;
-
-			if (val && val.then)
-				return val.then(
-					function (v) {
-						$this.$state = STATE_RESOLVED;
-						$this.$result = v;
-						//$this.$next();
-						nextTickDo(function () { $this.$fire(); });
-					},
-					function (e) {
-						$this.$reject(e);
-					}), undefined;
 
 			$this.$state = STATE_RESOLVED;
 			$this.$result = val;
@@ -119,66 +101,66 @@
 		}
 	}
 
-	// initial values into prototype (primitives only)
-	proto.$state = STATE_UNRESOLVED;
-	proto.$result = undefined;
-	proto.$handled = false;
-
 	// then
 	proto.then = function then(resolved, rejected) {
-		return new PromiseCore(PROMISE_THEN, rejected, resolved, null, this);
+		return new PromiseCore(PROMISE_THEN, rejected, resolved, this);
 	};
 
 	// catch
 	proto['catch'] = function caught(rejected) {
-		return new PromiseCore(PROMISE_THEN, rejected, null, null, this);
+		return new PromiseCore(PROMISE_THEN, rejected, null, this);
 	};
 
 	// fire
 	proto.$fire = function fire() {
-		//if (this.$state === STATE_UNRESOLVED) return;
 		var $this = this;
 		var $state = this.$state;
 		var $result = this.$result;
 		var $callbacks = this.$callbacks;
 		var elem;
 
+		if ($result instanceof Error) $state = STATE_REJECTED;
+
 		while (elem = $callbacks.shift()) {
 			(function (elem) {
-				var resolve = elem[ARGS_RESOLVE], reject = elem[ARGS_REJECT];
-				var r;
-				var cb = elem[ARGS_CALLBACK];
+				var resolve = elem[ARGS_RESOLVE];
+				var reject = elem[ARGS_REJECT];
 				var completed = elem[$state];
-				//function complete(val) {
-				//	resolve(completed.call($this, val)); }
+
+				function complete(val) {
+					try {
+						resolve(completed(val));
+					} catch (err) {
+						if ($state === STATE_REJECTED)
+							console.error(colors.purple(
+								'error in handler: ') + $this +
+								colors.purple(': ' + (val && val.stack || val)));
+						reject(err);
+					}
+				}
 
 				$this.$handled = true;
 
 				try {
-					if (completed)
-						r = completed.call(null, $result);
-					else if (cb)
-						r = cb.apply(null,
-							$state === STATE_RESOLVED ?
-								[null, $result] : [$result]);
-					else if ($state === STATE_REJECTED)
-						return reject($result);
-					else
-						return resolve();
+					if ($state === STATE_RESOLVED) {
+						if (!completed)
+							return resolve($result);
 
-					if (typeof r === 'function')
-						r(function (e, v) { return e ? reject(e) : resolve(v); });
-					else
-						resolve(r);
-				} catch (e) {
-					try {
-						reject(e);
+						if ($result && $result.then)
+							return $result.then(complete, reject);
+
+						if (typeof $result === 'function')
+							return $result(function (e, v) {
+								return e ? reject(e) : resolve(v);
+							});
 					}
-					catch (e2) {
-						console.error(colors.purple(
-							'error in handler: ' +
-							$this + ': ' + e.stack + '\n' + e2.stack));
+					else { // $state === STATE_REJECTED
+						if (!completed)
+							return reject($result);
 					}
+					complete($result);
+				} catch (err) {
+					reject(err);
 				}
 			})(elem);
 
