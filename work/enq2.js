5128f4cc5ea765a9a25681aa5b6589c53c9d941b
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-01-04 07:18:11 +0900

    enq2

---
+++ work/enq2.js
@@ -0,0 +1,29 @@
+void function () {
+	'use strict';
+
+	// Queue
+	function Queue() {
+		//if (!(this instanceof Queue)) return new Queue();
+		this.pos = this.len = 0;
+		this.que = [];
+	}
+	Queue.prototype.push = function push(x) {
+		this.que[this.len++] = x;
+	};
+	Queue.prototype.shift = function shift() {
+		if (this.pos < this.len) {
+			var x = this.que[this.pos];
+			this.que[this.pos] = undefined;
+			this.pos++;
+			return x;
+		}
+	};
+	Queue.prototype.clear = function clear() {
+		//console.log('enq2:', this.len);
+		this.pos = this.len = 0;
+		this.que = [];
+	};
+
+	if (typeof module === 'object' && module && module.exports)
+		module.exports = Queue;
+}();
